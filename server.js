b74ffc4c981af29c129423f37232827a82d8024c
shivani10397 <30874734+shivani10397@users.noreply.github.com>
2017-08-16 22:22:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res){
- ressendFile(createTemplate(articleOne));
+ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res){
  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));   
